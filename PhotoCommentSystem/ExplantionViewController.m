e72f18ece5ff73f3868a9ec8b081a3aacf0f46f9
YI-YING <mcucsie@MCUCSIEdeMacBook-Pro.local>
2016-01-05 11:27:38 +0800

    2016/1/5

---
+++ PhotoCommentSystem/ExplantionViewController.m
@@ -0,0 +1,37 @@
+//
+//  ExplantionViewController.m
+//  PhotoCommentSystem
+//
+//  Created by CSIE on 2015/10/21.
+//  Copyright © 2015年 CSIE. All rights reserved.
+//
+
+#import "ExplantionViewController.h"
+
+@interface ExplantionViewController ()
+
+@end
+
+@implementation ExplantionViewController
+
+//------------------------------------------------------------------------------------------------
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [_uiscrollviewScrollView setContentSize:CGSizeMake(320, 2000)];
+}
+
+//------------------------------------------------------------------------------------------------
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//------------------------------------------------------------------------------------------------
+
+@end
---
+++ PhotoCommentSystem/RootListTableViewController.h
@@ -0,0 +1,29 @@
+//
+//  RootListTableViewController.h
+//  PhotoCommentSystem
+//
+//  Created by CSIE on 2015/10/8.
+//  Copyright © 2015年 CSIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GridViewCollectionViewController.h"
+
+@interface RootListTableViewController : UITableViewController <UITextFieldDelegate, ThemeChange>
+
+//-------------------------------------------------------------------------------------------------------------------
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *uibarbuttonitemExplantion;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *uibarbuttonitemEdit;
+
+//-------------------------------------------------------------------------------------------------------------------
+
+//Theme
+@property NSUInteger nsuintegerTheme;
+
+//-------------------------------------------------------------------------------------------------------------------
+
+- (IBAction)newAlbum:(id)sender;
+- (IBAction)EditAlbum:(id)sender;
+
+@end
