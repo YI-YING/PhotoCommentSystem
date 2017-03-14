fd8ce84b40e44febf4feb17f2f38a64c7a032842
YI-YING <a3852870@gmail.com>
2017-03-14 17:37:55 +0800

    Bug fix

--- PhotoCommentSystem/ExplantionViewController.m
+++ PhotoCommentSystem/ExplantionViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "ExplantionViewController.h"
+#import "GuideView.h"
 
-@interface ExplantionViewController ()
-
-@end
+#define GuideView_Height 470
+#define GuideView_Width 320
 
 @implementation ExplantionViewController
 
@@ -19,17 +19,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+        
+    GuideView *gvAddTo = [[GuideView alloc] initWithTitle:@"新增到"
+                                              description:@"新增相片到不同的資料夾"
+                                                    image:[UIImage imageNamed:@"Create"]];
     
-    [_uiscrollviewScrollView setContentSize:CGSizeMake(320, 2000)];
-}
+    gvAddTo.frame = CGRectMake(0, 0, GuideView_Width, GuideView_Height);
 
-//------------------------------------------------------------------------------------------------
+    GuideView *gvSet = [[GuideView alloc] initWithTitle:@"設定"
+                                            description:@"在這裡面可以設定字體大小、顏色、字型"
+                                                  image:[UIImage imageNamed:@"Set"]];
+    gvSet.frame = CGRectMake(0, GuideView_Height + 10, GuideView_Width, GuideView_Height);
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    GuideView *gvComment = [[GuideView alloc] initWithTitle:@"註解"
+                                            description:@"在這裡能進行回憶的保存，與珍藏"
+                                                  image:[UIImage imageNamed:@"Comment"]];
+    gvComment.frame = CGRectMake(0, (GuideView_Height + 10) * 2, GuideView_Width, GuideView_Height);
+
+    GuideView *gvDelete = [[GuideView alloc] initWithTitle:@"刪除"
+                                            description:@"刪除照片"
+                                                  image:[UIImage imageNamed:@"Delete"]];
+    gvDelete.frame = CGRectMake(0, (GuideView_Height + 10) * 3, GuideView_Width, GuideView_Height);
+
+    
+    [self.uiscrollviewScrollView setContentSize:CGSizeMake(320, 1920)];
+
+    [self.uiscrollviewScrollView addSubview:gvAddTo];
+    [self.uiscrollviewScrollView addSubview:gvSet];
+    [self.uiscrollviewScrollView addSubview:gvComment];
+    [self.uiscrollviewScrollView addSubview:gvDelete];
+    
 }
 
 //------------------------------------------------------------------------------------------------
